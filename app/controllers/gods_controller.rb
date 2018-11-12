8b137bc382f3c458c500e649bbfb878e9b7889f7
alexandre minot <alexandre.minot@hotmail.fr>
2018-11-12 18:25:55 +1100

    Gods show and index + name to Users added

--- app/controllers/gods_controller.rb
+++ app/controllers/gods_controller.rb
@@ -1,8 +1,6 @@
 class GodsController < ApplicationController
 	skip_before_action :authenticate_user!, only: :index
-	has_many :bookings
-	belongs_to :user
-	
+
   def index
     @gods = God.all
   end
