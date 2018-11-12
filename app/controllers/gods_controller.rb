288d64cd38f9fdd37ae4ede6cd52782be5fa40e4
alexandre minot <alexandre.minot@hotmail.fr>
2018-11-12 17:25:02 +1100

    Fixes controllers and models + create index and show in GodsController

--- app/controllers/gods_controller.rb
+++ app/controllers/gods_controller.rb
@@ -1,4 +1,13 @@
 class GodsController < ApplicationController
-	has_many :bookings
-	belongs_to :user
+  def index
+    @gods = God.all
+  end
+
+  def show
+    @god = God.find(params[:id])
+  end
+
+  def god_params
+    params.require(:god).permit(:name, :location, :description)
+  end
 end
