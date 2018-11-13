2ceaf62896fbcbf918440494bf49d094aa10ffc2
alexandre minot <alexandre.minot@hotmail.fr>
2018-11-13 11:33:44 +1100

    seed + linking show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,14 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+  User.destroy_all
+  God.destroy_all
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  alex = User.create!(name: 'Alex', email: 'jean.luc@hotmail.fr', password: '*************')
+  buddha = God.new(name: 'Buddha', location: 'Lhassa', description: 'Big and fat')
+  buddha.user = alex
+  buddha.save
+
+# user_id: user.id, god_id: god.id,
