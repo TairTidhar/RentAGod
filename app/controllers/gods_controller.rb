beba5c91a3ce8c13e8d7665aebd571d5347f7aad
alexandre minot <alexandre.minot@hotmail.fr>
2018-11-14 12:04:20 +1100

    Display the image on the index page

--- app/controllers/gods_controller.rb
+++ app/controllers/gods_controller.rb
@@ -1,5 +1,5 @@
 class GodsController < ApplicationController
-	skip_before_action :authenticate_user!, only: :index
+	skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     if params[:search] != nil
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
+
+
   def home
-  	redirect_to gods_path
   end
 end
