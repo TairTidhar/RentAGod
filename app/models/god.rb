9e82dbaf17ca793ad7b37cf67b4fb484e63661cc
Alix Palmer <alix.palmer@hotmail.com>
2018-11-15 14:27:07 +1100

    So many changes

--- app/models/god.rb
+++ app/models/god.rb
@@ -1,9 +1,8 @@
 class God < ApplicationRecord
-  has_many :bookings, dependent: :destroy, through: :bookings
+  has_many :bookings, dependent: :destroy
   belongs_to :user
 
   validates :name, presence: true, uniqueness: true
   validates :location, :photo, :price, presence: true
   mount_uploader :photo, PhotoUploader
 end
-
